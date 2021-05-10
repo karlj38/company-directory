63c7851ed4fe4eda08e4f37f09ee9d9b7a6e4aad
Karl Jones <karlj38@gmail.com>
2021-05-10 21:56:38 +0000

    build nav menu

--- js/script.js
+++ js/script.js
@@ -6,6 +6,16 @@ function closeMenuBar() {
   $("#menuBar, #overlay").removeClass("active");
 }
 
+function configAdvSearch() {
+  //get dynamic fields
+  $("#expandedAdvSearch").slideDown();
+}
+
 function openMenuBar() {
   $("#menuBar, #overlay").addClass("active");
+}
+
+function toggleAdvSearch() {
+  $("#toggleAdvSearch i").toggleClass("rotate90");
+  $("#advSearch").slideToggle("fast");
 }
