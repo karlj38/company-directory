3b741ba5016677323428733e643fa1d2794a5200
Karl Jones <karlj38@gmail.com>
2021-05-11 23:23:14 +0000

    get locations

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ function getDepartment(id) {
         const d = data.data[0];
         $("#deptName").val(d.name);
         $("#deptLoc").val(d.location);
-        $("#viewDeptModal").modal("show");
+        $("#DeptModal").modal("show");
       } else {
         alert("No result found");
       }
@@ -67,7 +67,21 @@ function getEmployee(id) {
         $("#empJob").val(emp.jobTitle);
         $("#empEmail").val(emp.email);
         $("#empDept").val(emp.department);
-        $("#viewEmpModal").modal("show");
+        $("#EmpModal").modal("show");
+      } else {
+        alert("No result found");
+      }
+    }
+  });
+}
+
+function getLocation(id) {
+  $.getJSON("php/get", { type: "location", id: id }, function (data, status) {
+    if (data.status.code == 200) {
+      if ((data.data || null) && data.data.length) {
+        const l = data.data[0];
+        $("#locName").val(l.name);
+        $("#LocModal").modal("show");
       } else {
         alert("No result found");
       }
@@ -92,6 +106,7 @@ function getDepartments() {
 
         let $cardBody = $(`<div class="card-body"></div>`);
         $cardBody.append(`<p class="card-subtitle">${d.location}</p>`);
+        $cardBody.append(`<p class="card-text">Personnel: ${d.personnel}</p>`);
         $card.append($cardBody);
 
         let $cardFooter = $(`<div class="card-footer text-end"></div>`);
@@ -143,6 +158,42 @@ function getEmployees() {
         );
         $cardFooter.append(
           `<button class="btn btn-secondary btn-sm" onclick="configEmpModal(event, ${e.id})"><i class="fa fa-user-edit"></i></button>`
+        );
+        $card.append($cardFooter);
+
+        $col.append($card);
+        $("#grid").append($col);
+        closeMenuBar();
+      });
+    }
+  });
+}
+
+function getLocations() {
+  $.getJSON("php/getList", { type: "locations" }, function (data) {
+    const locs = data.data || null;
+    if (data.status.code == 200 && locs && locs.length) {
+      $("#grid").empty();
+      locs.forEach(function (l) {
+        let $col = $(
+          `<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-4"></div>`
+        );
+        let $card = $(`<div id="e${l.id}" class="card mx-auto"></div>`);
+
+        let $cardHeader = $(`<div class="card-header"></div>`);
+        $cardHeader.append(`<h2 class="card-title fs-4">${l.name}</h2>`);
+        $card.append($cardHeader);
+
+        let $cardBody = $(`<div class="card-body"></div>`);
+        $cardBody.append(`<p class="card-text">Personnel: ${l.personnel}</p>`);
+        $card.append($cardBody);
+
+        let $cardFooter = $(`<div class="card-footer text-end"></div>`);
+        $cardFooter.append(
+          `<button class="btn btn-primary btn-sm me-2"><i class="fas fa-eye"></i></button>`
+        );
+        $cardFooter.append(
+          `<button class="btn btn-secondary btn-sm" onclick="getLocation(${l.id})"><i class="fa fa-edit"></i></button>`
         );
         $card.append($cardFooter);
 
