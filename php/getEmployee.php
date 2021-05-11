edd80ea2a087db4947c3d278e89c2d762ad796b7
Karl Jones <karlj38@gmail.com>
2021-05-11 17:09:40 +0000

    return employee id

--- php/getEmployee.php
+++ php/getEmployee.php
@@ -4,7 +4,7 @@ include "config.php";
 if ($id = $_GET["id"] ?? null) {
     $id = escape($id);
     if (preg_match("/[0-9]{1,6}/", $id)) {
-        $query = "SELECT p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location
+        $query = "SELECT p.id, p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location
 FROM personnel p LEFT JOIN department d ON (d.id = p.departmentID)
 LEFT JOIN location l ON (l.id = d.locationID)
 WHERE p.id = '$id'
