c8f5b17cf4906d4d068328a0cc1a3c048b6f1162
Karl Jones <karlj38@gmail.com>
2021-05-12 12:48:22 +0000

    return columns for adv search form

---
+++ php/getColumns.php
@@ -0,0 +1,18 @@
+<?php
+include "config.php";
+
+if ($table = $_GET["table"] ?? null) {
+    if ($table == "department" || $table == "location" || $table == "personnel") {
+        $query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='companydirectory' AND TABLE_NAME='$table'";
+        $result = db($query);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            array_push($data, $row);
+        }
+        json(200, "ok", "success", $data);
+    } else {
+        json(400, "bad request", "invalid table");
+    }
+} else {
+    json(400, "bad request", "no table");
+}
